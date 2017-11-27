22d91769b3d943e3c85921e4ee89016a897154cb
psimoes <p.simoes@campus.fct.unl.pt>
2017-11-27 20:49:30 +0000

    need to beautify this

--- actions/query.go
+++ actions/query.go
@@ -5,17 +5,18 @@ import (
 	"reflect"
 
 	"github.com/gobuffalo/buffalo"
-	"github.com/gobuffalo/buffalo/render"
 )
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func QueryHandler(c buffalo.Context) error {
 	x := reflect.ValueOf(c.Params()).MapKeys()
-
-	for k, v := range x {
-		fmt.Println("%s %s", k, v)
+	values := []string{}
+	for _, v := range x {
+		fmt.Println("%T", v)
+		values = append(values, fmt.Sprintf("%+v", v))
 	}
+	c.Set("values", values)
 
-	return c.Render(200, render.String(fmt.Sprintf("%v\n", c.Params())))
+	return c.Render(200, r.HTML("query.html"))
 }
